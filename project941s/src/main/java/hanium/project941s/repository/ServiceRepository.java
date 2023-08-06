96f2c09feb4e7bf3180b575d9d4bb00ddb6b04a3
pcg0610 <changyu9906@gmail.com>
2023-08-06 20:31:05 +0900

    테이블 설정 페이지 구현

--- project941s/src/main/java/hanium/project941s/repository/ServiceRepository.java
+++ project941s/src/main/java/hanium/project941s/repository/ServiceRepository.java
@@ -3,9 +3,12 @@ package hanium.project941s.repository;
 import hanium.project941s.domain.MemberService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ServiceRepository extends JpaRepository<MemberService, Long> {
 
     public MemberService getOne(Long id);
+
+    public List<MemberService> findByMember_Id(Long id);
 }
